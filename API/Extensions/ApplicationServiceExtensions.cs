84f25fee4a8723a459975e392a3ce5efc1a4cf6f
Kenneth Brian <xkenntsx619@gmail.com>
2021-12-21 13:25:52 +0800

    End of Section 2

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,20 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
